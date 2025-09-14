24679b7777b915f9b5efa4cc50f0627efb41e023
Tarasov Maksym <maksimmm45555@gmail.com>
2025-09-15 02:42:56 +0300

    solved py-cache-decorator task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    final_list = {}
+
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        if args in final_list:
+            print("Getting from cache")
+            return final_list[args]
+        print("Calculating new result")
+        result = func(*args)
+        final_list[args] = result
+        return result
+    return wrapper
