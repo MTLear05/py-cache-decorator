b166df5bf1ffe85407e456a691d5ec30c624a42c
Tarasov Maksym <maksimmm45555@gmail.com>
2025-09-15 02:55:42 +0300

    fixed names in py-cache-decorator task(**kwargs)

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Callable, Any
 def cache(func: Callable) -> Callable:
     cache_dict = {}
 
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
+    def wrapper(*args: Any) -> Any:
         if args in cache_dict:
             print("Getting from cache")
             return cache_dict[args]
